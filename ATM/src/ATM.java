74c01e57fd2300e29816ea833832165485f73c55
yearmfew <brlylmz23@gmail.com>
2019-08-10 16:42:25 +0200

    para çekme fonksiyonu üzerinde değişiklikler yaptım. yaptığım değişklikleri yorum satırları içierisinde yazdım. Githubı karıştırırsan benim değiştirdiğim dosya ile orjinal dosya arasındaki farkları inceleyebildiğin bir kısım var. Baya işe yarar kullanışlı bir özellik.

--- ATM/src/ATM.java
+++ ATM/src/ATM.java
@@ -1,64 +1,71 @@
 
 import java.util.Scanner;
 
-
 public class ATM {
-    public void calis(Hesap hesap){
-    Login login=new Login();
-    
-    Scanner scanner=new Scanner(System.in);
+
+    public void calis(Hesap hesap) {
+        Login login = new Login();
+
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Bankamıza Hoş geldiniz...");
         System.out.println("***********************************");
-        int giris_hakki=3;
-        while(true){
-        if(login.login(hesap))
-        {
-            System.out.println("Giriiş başarılı..");
-            break;
-        }
-        else{
-            System.out.println("opps!! yanlış giriş");
-            giris_hakki-=1;
-            System.out.println("Kalan giriş hakkınız: "+giris_hakki);
-        }
-        if(giris_hakki==0){
-            System.out.println("Giriş hakkınız bitti...");
-        return;
-        }
+        int giris_hakki = 3;
+        while (true) {
+            if (login.login(hesap)) {
+                System.out.println("Giriiş başarılı..");
+                break;
+            } else {
+                System.out.println("opps!! yanlış giriş");
+                giris_hakki -= 1;
+                System.out.println("Kalan giriş hakkınız: " + giris_hakki);
+            }
+            if (giris_hakki == 0) {
+                System.out.println("Giriş hakkınız bitti...");
+                return;
+            }
             System.out.println("***************************************");
         }
-        String islemler="Yapmak istediğiniz işlemi seçiniz\n"+
-                "1 - Bakiye Görüntüle\t"+
-                "2 - Para Yatırma\n"+
-                "q - Çıkış\t\t"+
-                "3 - Para Çek\n";
+        String islemler = "Yapmak istediğiniz işlemi seçiniz\n"
+                + "1 - Bakiye Görüntüle\t"
+                + "2 - Para Yatırma\n"
+                + "q - Çıkış\t\t"
+                + "3 - Para Çek\n";
         System.out.println(islemler);
-        
-        while(true){
+
+        while (true) {
             System.out.print("İşlem Seçiniz: ");
-            String islem=scanner.nextLine();
-            
-            if(islem.equals("q")){
+            String islem = scanner.nextLine();
+
+            if (islem.equals("q")) {
                 System.out.println("Çıkış yaptınız.. Tekrar bekleriz..");
                 break;
-            }
-            else if(islem.equals("1")){
-                System.out.println("Hesap bakiyeniz: "+ hesap.getBakiye());
-            }
-            else if(islem.equals("2")){
+            } else if (islem.equals("1")) {
+                System.out.println("Hesap bakiyeniz: " + hesap.getBakiye());
+            } else if (islem.equals("2")) {
                 System.out.print("Yatırmak istediğiniz miktar: ");
-                int tutar=scanner.nextInt();
+                int tutar = scanner.nextInt();
                 scanner.nextLine();
                 hesap.paraYatir(tutar);
-            }
-            else if(islem.equals("3")){
-            
+            } else if (islem.equals("3")) {
+
                 System.out.print("Çekmek istediğiniz miktar: ");
-                int tutar=scanner.nextInt();
+                int tutar = scanner.nextInt();
                 scanner.nextLine();
-                hesap.paraCek(tutar);
-            }
-            else{
+                boolean result = hesap.paraCek(tutar);
+                if (result) {
+                    // bu result değerini ilk başta ne dönüyor anlamak için 
+                    //yazdırdım sonra da silmedim belki sende bakmak istersin diye.
+                    System.out.println(result);
+                    // burada true dönüyor yani if içine 1 girdiği için bu bloğa giriyor program. 
+                    System.out.println("İşlem başarılı");
+                    System.out.println("Kalan bakiye miktarı : " + hesap.getBakiye() );
+
+                } else {
+                    System.out.println(result);
+                    // burada false dönüyor ve biz uyarı için istediğimiz görsel vs uygulamamızda ekliyoruz. 
+                    System.out.println("Hesabınızda bu kadar para bulunmamakradır....");
+                }
+            } else {
                 System.out.println("Geçersiz seçenek");
             }
         }
--- ATM/src/Main.java
+++ ATM/src/Main.java
@@ -2,7 +2,12 @@
 public class Main {
     public static void main(String[] args) {
         ATM atm=new ATM();
-        Hesap hesap= new Hesap("ismail", "12345", 2000);
+        Hesap hesap= new Hesap("ismail", "1", 5000);
+    // ikinci bir hesap eklemeye çalıştım olmadı. onun için daha gelişmiş bir sistem lazım.
+    // normalde veritabanından bu işlemleri yaparız ama burada bir veri tabanımız yok
+    // bunun yerine sınıfları kullanarak veritabanı benzeri bir şey yapabilir miyiz?
+    // bir kaç tane hesap ekleyebilmek ve bunun kontrolünü yapabilmek için
+      
         
         atm.calis(hesap);
         System.out.println("Sistemden çıktınız..");
